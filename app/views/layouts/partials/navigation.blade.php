2c63272b23d1297e9e0541273874c68e1c68bdf7
i072332 <deniz.murat@sap.com>
2014-06-18 10:45:32 +0300

    re-arrange nav

--- app/views/layouts/partials/navigation.blade.php
+++ app/views/layouts/partials/navigation.blade.php
@@ -5,34 +5,23 @@
 		    <li class="{{Request::path() == 'home' ? 'active' : '';}}" ><a href="{{ URL::to('home') }}">pps</a></li>
 		    <li class="{{Request::path() == 'ppm' ? 'active' : '';}}"><a href="{{URL::to('/ppm')}}">ppm</a></li>
 		    <li class="{{Request::path() == 'livescore' ? 'active' : '';}}"><a href="{{URL::to('/livescore')}}">livescore</a></li>
-		    <li class="{{Request::path() == 'poolmanagement' ? 'active' : '';}}"><a href="{{URL::to('/poolmanagement')}}">pool management</a></li>
-		    <li class="{{Request::path() == 'countries' ? 'active' : '';}}"><a href="{{ URL::to('countries') }}">stats</a></li>
-		    
+		    <li class="{{Request::path() == 'poolmanagement' ? 'active' : '';}}"><a href="{{URL::to('/poolmanagement')}}">pool</a></li>
 		    <li class="dropdown">
 		      <a href="#" class="dropdown-toggle" data-toggle="dropdown">tools <b class="caret"></b></a>
 		      <ul class="dropdown-menu">
-		        <li><a href="{{URL::to('/simulator')}}">sim</a></li>
-		        <li><a href="{{URL::to('/addleagues')}}">add leagues</a></li>
-		        <li><a href="{{URL::to('/simulatormerged')}}">merged sim</a></li>
-		        <li class="divider"></li>
-		        <li class="dropdown-header">Nav header</li>
-		        <li><a href="#">Separated link</a></li>
-		        <li><a href="#">One more separated link</a></li>
+                  <li class="{{Request::path() == 'countries' ? 'active' : '';}}"><a href="{{ URL::to('countries') }}">stats</a></li>
+                <li class="divider"></li>
+                <li class="dropdown-header">Simulators</li>
+                  <li><a href="{{URL::to('/simulator')}}">sim</a></li>
+                  <li><a href="{{URL::to('/simulatormerged')}}">merged sim</a></li>
+                  <li class="divider"></li>
+                  <li class="dropdown-header">Admin</li>
+                  <li><a href="{{URL::to('/addleagues')}}">add leagues</a></li>
 		      </ul>
 		    </li>
 		  </ul>
 		  <ul class="nav navbar-nav navbar-right">
 		    <li class="dropdown">
-<!-- 		      <a href="#" class="dropdown-toggle" data-toggle="dropdown">dropdown <b class="caret"></b></a>
-		      <ul class="dropdown-menu">
-		        <li><a href="#">Action</a></li>
-		        <li><a href="#">Another action</a></li>
-		        <li><a href="#">Something else here</a></li>
-		        <li class="divider"></li>
-		        <li class="dropdown-header">Nav header</li>
-		        <li><a href="#">Separated link</a></li>
-		        <li><a href="#">One more separated link</a></li>
-		      </ul> -->
 		    </li>
 		    @if(isset($pool))
 		    <li><p class="navbar-text"><span class="text-default">P: {{$pool->amount}} (<strong>{{$pool->current}}</strong>)</span></p></li>
