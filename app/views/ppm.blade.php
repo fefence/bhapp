5ba5d4c41650b45041ace5e3975b59463f1b4ec5
i072332 <deniz.murat@sap.com>
2014-08-13 16:02:37 +0300

    ppm table visuals

--- app/views/ppm.blade.php
+++ app/views/ppm.blade.php
@@ -74,11 +74,11 @@ $i = 0;
             @endif
         </td>
         <td>{{$d->resultShort}}</td>
-        <td @if($d->resultShort == '-') class='editable text-warning' @else class='text-warning' @endif>{{$d->type}}</td>
+        <td class='text-muted'><em>{{$d->type}}</em></td>
 <!--        <td class='editabledd warning'>{{$d->bookmakerName}}</td>-->
         <td>{{$d->bsf}}</td>
         <td @if($d->resultShort == '-') class='editable' @endif id="{{$d->game_type_id}}">{{$d->bet}}</td>
-        <td @if($d->resultShort == '-') class='editable alert-warning' @endif id="{{$d->game_type_id}}">{{$d->odds}}</td>
+        <td @if($d->resultShort == '-') class='editable oddsColumn' @endif id="{{$d->game_type_id}}">{{$d->odds}}</td>
         <td>{{$d->income}}</td>
         <td>{{round(($d->income - $d->bsf - $d->bet), 2, PHP_ROUND_HALF_UP)}}</td>
         <td>@if($d->resultShort == '-')  <a href="/confirm/{{$d->games_id}}/{{$d->game_type_id}}" style="font-size: 130%;">+&nbsp<span style='display: none;'>{{$d->match_id}}</span></a>({{ (array_key_exists($d->match_id, $count))?$count[$d->match_id]:$count[$d->id] }})
