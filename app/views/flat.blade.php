05647b7a0e7ba9eeecc90b0b9dfeef35ed30f66e
veseto <wpopowa@gmail.com>
2014-08-26 12:57:55 +0300

    fix ppm link in flat view

--- app/views/flat.blade.php
+++ app/views/flat.blade.php
@@ -4,7 +4,7 @@
 	<!-- breadcrumbs -->
 
 	<?php
-		$list = array('ppm' => URL::to("home"));
+        $list = array('ppm' => URL::to("/ppm"));
 		$active = 'flat';
 		$elements = array('active' => $active, 'list' => $list);
 	?>
