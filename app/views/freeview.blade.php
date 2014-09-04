4ae2f069363607399449ca9e29513131b6212786
veseto <wpopowa@gmail.com>
2014-09-04 20:59:09 +0300

    blue button to become default when something is confirmed

--- app/views/freeview.blade.php
+++ app/views/freeview.blade.php
@@ -90,7 +90,7 @@ $elements = array('active' => $active, 'list' => $list);
         <td class='editable' id="{{$d->team_id}}">{{$d->odds}}</td>
         <td>{{$d->income}}</td>
         <td>{{round(($d->income - $d->bsf - $d->bet), 2, PHP_ROUND_HALF_UP)}}</td>
-        <td><a role="button" class="btn btn-primary btn-xs" style="width: 50px" href="/confirmfree/{{$d->games_id}}">+&nbsp({{ (array_key_exists($d->match_id, $count))?$count[$d->match_id]:$count[$d->id] }})<span style='display: none;'>{{$d->match_id}}#{{$d->team_id}}</span></a>
+        <td><a role="button" @if ($count[$d->id] != 0) class="btn btn-default btn-xs" @else class="btn btn-primary btn-xs" @endif style="width: 50px" href="/confirmfree/{{$d->games_id}}">+&nbsp({{ (array_key_exists($d->match_id, $count))?$count[$d->match_id]:$count[$d->id] }})<span style='display: none;'>{{$d->match_id}}#{{$d->team_id}}</span></a>
         </td>
     </tr>
 
