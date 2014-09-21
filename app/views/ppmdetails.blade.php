fc2cfaaf0e22961b5856a143b2bf2c05606068c5
veseto <wpopowa@gmail.com>
2014-09-21 16:42:40 +0300

    prettier ppm details

--- app/views/ppmdetails.blade.php
+++ app/views/ppmdetails.blade.php
@@ -7,12 +7,11 @@
             @include('layouts.partials.square', ['data' => ['match'=>$match], 'game_type' => $type])
         @endforeach
     </div>
+    <div class="col-xs-12">
+        @foreach($games as $game)
+            [BSF: {{$game->bsf}}] {{$game->bet}} @ {{$game->odds}} {{$game->income}} <a role="button" class="btn btn-xs w25 btn-danger" href="/delete{{$isFree}}/{{$game->id}}/{{$game->game_type_id}}">-</a>
+        @endforeach
+    </div>
 </div>
-<table class="noMargin noPadding">
-    @foreach($games as $game)
-    <tr>
-        <td>[BSF: {{$game->bsf}}] {{$game->bet}} @ {{$game->odds}} {{$game->income}} <a role="button" class="btn btn-xs w25 btn-danger" href="/delete{{$isFree}}/{{$game->id}}/{{$game->game_type_id}}">-</a></td>
-    </tr>
-    @endforeach
-</table>
+
 @stop
